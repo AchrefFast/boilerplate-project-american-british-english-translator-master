53b76d4bff6b6a07ca02f51d05aeb6dd1503817e
AchrefFast <justnevergiveup001@gmail.com>
2021-08-24 02:29:37 +0100

    Added a comment

--- components/translator.js
+++ components/translator.js
@@ -3,6 +3,7 @@ const americanToBritishSpelling = require('./american-to-british-spelling.js');
 const americanToBritishTitles = require("./american-to-british-titles.js")
 const britishOnly = require('./british-only.js')
 
+// This "class" will be handling the translation of words from American English to British English and vice versa.
 class Translator {
 
 }
