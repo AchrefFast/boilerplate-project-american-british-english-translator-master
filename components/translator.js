d49de7b89f8993aaefa4cc5f48d775f5a10f8204
AchrefFast <justnevergiveup001@gmail.com>
2021-08-27 00:39:18 +0100

    Passed FCC test

--- components/translator.js
+++ components/translator.js
@@ -2,10 +2,71 @@ const americanOnly = require('./american-only.js');
 const americanToBritishSpelling = require('./american-to-british-spelling.js');
 const americanToBritishTitles = require("./american-to-british-titles.js")
 const britishOnly = require('./british-only.js')
+const amSpell = Object.keys(americanToBritishSpelling);
+const brSpell = Object.values(americanToBritishSpelling);
+const amTitles = Object.keys(americanToBritishTitles);
+const brTitles = Object.values(americanToBritishTitles);
+const amOnlyKeys = Object.keys(americanOnly);
+const brOnlyKeys = Object.keys(britishOnly);
 
-// This "class" will be handling the translation of words from American English to British English and vice versa.
+// // This "class" will be handling the translation of words from American English to British English and vice versa.
 class Translator {
 
+    translate(string, toLanguage) {
+
+        const spanning = (word) => {
+            return '<span class="highlight">' + word + '</span>'
+        }
+
+        if (toLanguage.toLowerCase() == 'am') {
+
+            const regex_time = /(\d+?)([:])(\d+)/g;
+            string = string.replace(regex_time, spanning('$1.$3'));
+
+            for (let i = 0; i < amSpell.length; i++) {
+                let word = new RegExp("(?<!-)" + amSpell[i] + "(?!\\w)", 'gi');
+                string = string.replace(word, spanning(brSpell[i]));
+            }
+
+            for (let i = 0; i < amTitles.length; i++) {
+                let word = new RegExp(amTitles[i] + "(?!\\w)", 'gi');
+                let new_word = brTitles[i][0].toUpperCase() + brTitles[i].slice(1);
+                string = string.replace(word, spanning(new_word));
+            }
+
+            for (let i = 0; i < amOnlyKeys.length; i++) {
+                let word = new RegExp("(?<!-)" + amOnlyKeys[i] + "(?!\\w)", 'gi');
+                string = string.replace(word, spanning(americanOnly[amOnlyKeys[i]]));
+            }
+            return string;
+        }
+
+        if (toLanguage.toLowerCase() == 'br') {
+
+            const regex_time = /(\d+?)([.])(\d+)/g;
+            string = string.replace(regex_time, spanning('$1:$3'));
+
+            for (let i = 0; i < brSpell.length; i++) {
+                let word = new RegExp("(?<!-)" + brSpell[i] + "(?!\\w)", 'gi');
+                string = string.replace(word, spanning(amSpell[i]));
+            }
+
+            for (let i = 0; i < brTitles.length; i++) {
+                let word = new RegExp(brTitles[i] + "(?!\\w)", 'gi');
+                let new_word = amTitles[i][0].toUpperCase() + amTitles[i].slice(1);
+                string = string.replace(word, spanning(new_word));
+            }
+
+            for (let i = 0; i < brOnlyKeys.length; i++) {
+                let word = new RegExp("(?<!-)" + brOnlyKeys[i] + "(?!\\w)", 'gi');
+                string = string.replace(word, spanning(britishOnly[brOnlyKeys[i]]));
+            }
+            return string;
+        }
+    }
 }
 
 module.exports = Translator;
+const translator = new Translator();
+const input = "chip shop"
+console.log(translator.translate(input, 'br'));
--- routes/api.js
+++ routes/api.js
@@ -3,11 +3,35 @@
 const Translator = require('../components/translator.js');
 
 module.exports = function (app) {
-  
+
   const translator = new Translator();
+  const lang = {
+    'american-to-british': 'am',
+    'british-to-american': 'br'
+  }
 
   app.route('/api/translate')
     .post((req, res) => {
-      
+      const text = req.body.text;
+      const locale = req.body.locale;
+      if (text === '') {
+        return res.json({ error: 'No text to translate' });
+      }
+      else if (!text || !locale) {
+        return res.json({ error: 'Required field(s) missing' })
+      }
+      if (!['american-to-british', 'british-to-american'].includes(locale)) {
+        return res.json({ error: 'Invalid value for locale field' })
+      }
+      //console.log(lang[locale]);
+      //console.log('The text is: ', text);
+      let translation = translator.translate(text, lang[locale]);
+      //console.log("The result is: ", translation);
+      if (translation == text) {
+        return res.json({ text: text, translation: 'Everything looks good to me!' })
+      }
+      else {
+        return res.json({ text: text, translation: translation });
+      }
     });
 };
